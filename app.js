bd99875ba2cf2e2e2b622468e6e5d84ecbc9819c
Daniel <danielfcarmichael@gmail.com>
2023-12-09 21:10:02 -0600

    try catch wrapping

--- app.js
+++ app.js
@@ -18,22 +18,28 @@ app.get("/", (req, res) => {
 
 // this endpoint uses the "auth" auth
 app.post("/create_jam", async (req, res) => {
-  dbConnect(process.env.GEN_AUTH);
-
-  const { title, time_limit, jam_url, options = {} } = req.body;
-  const jam_id = uuidv4();
-  console.log("yes");
-  const new_jam = new Jam({
-    title,
-    time_limit,
-    created_timestamp: Date.now(),
-    jam_url,
-    options,
-    _id: jam_id,
-  });
-
-  await new_jam.save();
-  res.status(200).json({ message: "Jam Created" });
+  try {
+    dbConnect(process.env.GEN_AUTH);
+  
+    const { title, time_limit, jam_url, options = {} } = req.body;
+    const jam_id = uuidv4();
+    console.log("yes");
+    const new_jam = new Jam({
+      title,
+      time_limit,
+      created_timestamp: Date.now(),
+      jam_url,
+      options,
+      _id: jam_id,
+    });
+  
+    await new_jam.save();
+    res.status(200).json({ message: "Jam Created" });
+  }
+  catch (error) {
+    console.log("there was an error creating the authentication");
+    res.status(500).json({ message: error.message });
+  }
 });
 
 const port = process.env.PORT || 3000;
