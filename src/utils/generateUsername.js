3fb8b29625b895514ec950a1bab29adc924405e1
Daniel <danielfcarmichael@gmail.com>
2023-12-15 03:15:32 -0600

    update username  generation

--- src/utils/generateUsername.js
+++ src/utils/generateUsername.js
@@ -3,8 +3,7 @@ function generateUniqueUsername() {
   const randomNumber = Math.floor(1000000000 + Math.random() * 9000000000); // Generate a random 10-digit number
 
   // You can add a timestamp or a random string for extra uniqueness
-  const timestamp = Date.now();
-  const uniqueUsername = `${prefix}${randomNumber}_${timestamp}`;
+  const uniqueUsername = `${prefix}${randomNumber}`;
 
   return uniqueUsername;
 }
