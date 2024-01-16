2209b216b1f8be18468723278d39b1047e1a7930
Daniel <danielfcarmichael@gmail.com>
2024-01-16 11:17:34 -0600

    testing

--- src/models/folder.js
+++ src/models/folder.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const folderSchema = new mongoose.Schema({
   folder_id: { type: String, required: true },
   associated_org: { type: Object, required: true },
-  documents: { type: Object, required: true },
+  documents: { type: Array, required: true },
   description: { type: String, required: true },
   name: { type: String, required: true },
   created_by: { type: Object, required: true },
