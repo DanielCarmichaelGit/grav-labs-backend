ad1c35707668ea0ca02887a5125d5e6b0c561040
Daniel <danielfcarmichael@gmail.com>
2024-02-03 18:22:17 -0500

    document updating

--- app.js
+++ app.js
@@ -1159,6 +1159,7 @@ app.post("/autosave-document", authenticateJWT, async (req, res) => {
         { $set: document_data },
         { new: true } // Return the updated document
       );
+      console.log("document updated")
       document = existing_document;
     }
 
