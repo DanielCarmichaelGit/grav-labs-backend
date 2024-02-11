76973bd40f70eed053a7162a5ca85ca3ecd597c5
Daniel <danielfcarmichael@gmail.com>
2024-02-11 12:47:07 -0500

    remove org endpoint comments

--- app.js
+++ app.js
@@ -593,13 +593,9 @@ app.get("/organization", authenticateJWT, async (req, res) => {
 
     const user = req.user.user;
 
-    console.log("1", user);
-
     const organization = await Organization.findOne({
       org_id: user.organization.org_id,
     });
-
-    console.log("2", organization);
 
     if (organization) {
       res.status(200).json({
