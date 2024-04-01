4a59388bc499b8512ce81d237e80aa95cacc7414
Daniel <danielfcarmichael@gmail.com>
2024-04-01 15:33:09 -0400

    spool db connection

--- app.js
+++ app.js
@@ -79,11 +79,10 @@ app.get("/", (req, res) => {
 app.get("/user", authenticateJWT, async (req, res) => {
   try {
     const user_id = req.user.user.user_id;
-    console.log(req.user.user)
-    console.log(req.user)
-    console.log(user_id)
 
     if (user_id) {
+      await dbConnect(process.env.GEN_AUTH);
+      
       const existing_user = await User.findOne({ user_id });
 
       if (existing_user) {
