950478b58044195eb8def30754204b5a0e2ab4a4
Daniel <danielfcarmichael@gmail.com>
2024-04-10 16:07:07 -0400

    testing

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.options("*", cors());
 app.use(express.json({ limit: "50mb" }));
-app.timeout = 60 * 1000 * 10;
+app.setTimeout(10 * 60 * 1000);
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
